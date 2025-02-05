2f1617a94d0b9c4a5aad4b1d5b2f89954653a8ef
jove06 <alexiariccardi@gmail.com>
2025-02-04 20:18:36 -0500

    solved e10

--- exercises/e10.js
+++ exercises/e10.js
@@ -5,7 +5,8 @@ import { data } from "../data/data";
 // Return example: { key1: value1, key2: value2, ... , keyN: valueN }
 
 export function getAsteroidDataByName(data, asteroidName) {
-  // Your code goes here...
+  let asteroidData = data.asteroids.find(asteroid => asteroid.name.includes(asteroidName));
+  return asteroidData;
 }
 
 
